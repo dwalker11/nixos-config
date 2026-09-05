1b482b01637e85d55163657b0e9ce5b52d2d33f7
Devon Walker <dwalker11@users.noreply.github.com>
2026-09-05 14:09:26 -0400

    Update nix home-manager

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Home Manager configuration of devonwalker";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-25.05";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
